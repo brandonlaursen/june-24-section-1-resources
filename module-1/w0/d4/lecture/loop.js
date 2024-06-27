d7b1fe43e7d8ee5994ea39a3146ebba7efe9756f
brandon <brandonlaursen95@yahoo.com>
2024-06-27 10:50:54 -0400

    d4 notes

---
+++ module-1/w0/d4/lecture/loop.js
@@ -0,0 +1,20 @@
+
+/*
+
+  Loops
+
+  Loops provide us a way to repeat behavior a set number of times.
+  * We can use that behavior to progress through data structures and take a look at individual elements.
+  * Loops consist of 3 main components:
+    1. Initial Expression
+    2. Condition for which we keep looping
+    3. Step towards completing the loop/meeting the condition
+
+  With loops we control the boundaries!
+  There are may use cases of loops!
+    * counting numbers
+    * progressing through strings/arrays
+      * we loop through these data types to get information
+      * arrays can hold user information that we may need to access to print to the string
+
+*/
