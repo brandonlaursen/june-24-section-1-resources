89576f57dd86630a1e5c92a4f03fe255a5be8a5f
brandon <brandonlaursen95@yahoo.com>
2024-06-27 14:10:34 -0400

    lunch time

--- module-1/w0/d4/guided-practice/02-basic-loops/problems/04-elements-of-odd-indices-reversed.js
+++ module-1/w0/d4/guided-practice/02-basic-loops/problems/04-elements-of-odd-indices-reversed.js
@@ -4,13 +4,62 @@ returns a new string containing all the elements with odd indices in the input
 string in reverse order.
 */
 
+
+/*
+
+  input: 'academy'
+  output: 'mdc'
+  0 1 2 3 4 5 6
+  a c a d e m y
+    c   d   m
+    m   d   c
+
+  make a plan
+  1. define a function
+  2. define a newStr
+  3. iterate
+      a. start at index 6
+      b. stop at index 0
+      c. step in increments of 2, or 1 and use a condition to check if current index is even
+      d. as we iterate concatenate the current letter
+  4. return our newStr
+
+*/
+
 function elementsOfOddIndicesReversed(str) {
-  // Your code here 
+  // Your code here
+  let newStr = '';
+  // console.log(str[str.length - 1]);// y
+  for(let i =str.length - 1; i >= 0; i--){
+    let letter = str[i];
+    // console.log(letter);
+    if(i % 2 !== 0) {
+      // console.log(letter);
+      newStr += letter;
+    }
+  }
+
+  return newStr;
 }
 
+console.log(elementsOfOddIndicesReversed('academy')); // 'mdc'
+
+// function elementsOfOddIndicesReversed(str) {
+//   // Your code here
+//   let index = str.length - 1;
+//   let result = ""
+//   while (index >= 0) {
+//     let char = str[index];
+//     if ( index % 2 !== 0) {
+//       result += char;
+//     }
+//     index--
+//   }
+//   return result;
+// }
 // console.log(elementsOfOddIndicesReversed('academy')); // 'mdc'
-// console.log(elementsOfOddIndicesReversed('planet'));  // 'tnl'
-// console.log(elementsOfOddIndicesReversed('sport'));   // 'rp'
+console.log(elementsOfOddIndicesReversed('planet'));  // 'tnl'
+console.log(elementsOfOddIndicesReversed('sport'));   // 'rp'
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
--- module-1/w0/d4/guided-practice/02-basic-loops/problems/05-first-half.js
+++ module-1/w0/d4/guided-practice/02-basic-loops/problems/05-first-half.js
@@ -2,16 +2,53 @@
 Define a function called `firstHalf` that takes in a string
 parameter. The function should return a new string containing only the first
 half of characters in the string parameter.
+
+
+
+understand the problem
+  input: 'academy'
+  output: acad
+  * how do i get from my input to my output?
+  0 1 2 3 4 5 6
+  a c a d e m y
+
+  length = 7
+  7 / 2 = 3.5
+
+  make a plan
+  1. define a function
+  2. create a variable halfway that changes depending on if the string.length is odd or even
+    a. check if string is odd or even with condition using modulo  n % 2 === 0
+    b. edge case: check is there is no length
+    c. edge case: if the string length is 1
+  3. store the result in a string after using slice up to the halfway point
+  4. return result
+
+execute the plan
 */
 
+const firstHalf = str =>  str.slice(0, str.length % 2 === 0 ? str.length / 2 : (str.length + 1) / 2);
+// }
 function firstHalf(str) {
-  // Your code here 
+  // let halfway;
+  // if (str.length % 2 === 0) {
+  //   halfway = str.length / 2;
+  // } else {
+  //   halfway = (str.length + 1) / 2;
+  // }
+
+  // let halfway = str.length % 2 === 0 ? str.length / 2 : (str.length + 1) / 2
+
+  // console.log(halfway);
+  let result = str.slice(0, str.length % 2 === 0 ? str.length / 2 : (str.length + 1) / 2);
+  return str.slice(0, str.length % 2 === 0 ? str.length / 2 : (str.length + 1) / 2);
 }
 
-// console.log(firstHalf('academy')); // 'acad'
-// console.log(firstHalf('planet'));  // 'pla'
-// console.log(firstHalf('sport'));   // 'spo'
 
+
+console.log(firstHalf("academy")); // 'acad'
+console.log(firstHalf("planet")); // 'pla'
+console.log(firstHalf("sport")); // 'spo'
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = firstHalf;
