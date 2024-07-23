0284a25096d561828da3e373f3ff8e98027c0daa
brandon <brandonlaursen95@yahoo.com>
2024-07-23 11:29:06 -0400

    import

---
+++ module-2/w4/d2/lecture/commonJS/export.js
@@ -0,0 +1,52 @@
+/*
+
+ Imports and exports/common js modules
+
+ Common JS Modules
+  * In Node.js, each JavaScript file defines a module.
+  * Think of modules like an object - {}
+  * can see the object if you console.log(module)
+  * think about node_modules
+    * They are downloaded using npm install which installs dependencies within in the package.json
+    * Which then allow to import code from other files or Modules
+
+  * Exporting
+
+*/
+
+
+
+
+function add(num1, num2) {
+
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num2 - num1
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+
+// module.exports = {
+//   add: add,
+//   subtract: subtract,
+//   multiply: multiply
+// };
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+};
+
+
+// module.exports = add;
+
+// module.exports = [add, subtract, multiply];
+
+
+console.log(module);
---
+++ module-2/w4/d2/lecture/commonJS/import.js
@@ -0,0 +1,28 @@
+// const calculator = require('./export');
+
+const { add, subtract, multiply } = require("./export");
+
+
+const half = require('./folder-module/half')
+
+console.log(half);
+// console.log(calculator);
+
+
+// const [func1, func2, func3] = require('./export');
+
+
+// console.log(func1, func2, func3);
+// let add = calculator.add;
+// let subtract = calculator.subtract;
+// let multiply = calculator.multiply;
+
+// console.log(add); // [Function: add]
+
+
+/* {
+  add: [Function: add],
+  subtract: [Function: subtract],
+  multiply: [Function: multiply]
+}
+*/
