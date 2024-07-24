87db5dc44bc02aafcb90b4796e9f31b22f5c7adb
brandon <brandonlaursen95@yahoo.com>
2024-07-24 12:51:38 -0400

    context

--- module-2/w4/d3/guided-practice/02-bind/test.js
+++ module-2/w4/d3/guided-practice/02-bind/test.js
@@ -1,1 +1,23 @@
-// Your code here 
+const Employee = require('./employee');
+
+// console.log(Employee);
+
+const john = new Employee('John Wick','Dog Lover');
+
+
+// setTimeout(john.sayName, 2000);
+
+
+// setTimeout(() => john.sayName(), 2000);
+
+
+
+
+
+
+let sayNameBound = john.sayName.bind(john);
+
+// sayNameBound();
+
+
+setTimeout(sayNameBound, 2000);
