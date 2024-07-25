7abe035e84f6cf9e40e214edae13b3fb86f8df12
brandon <brandonlaursen95@yahoo.com>
2024-07-25 12:45:09 -0400

    error handling

--- module-2/w4/d4/guided-practice/01-error-handling/error-handling.js
+++ module-2/w4/d4/guided-practice/01-error-handling/error-handling.js
@@ -1,23 +1,53 @@
+
+
+
+
+// TypeError:
 // 1.
 function sum(array) {
   let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+
+  try {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+  } catch(e) {
+    // type
+    console.log(e.message);
   }
+
   return sum;
 }
 
-let res = sum(null);
-console.log(res);
+// let res = sum(null);
+// console.log(res);
 
 // 2.
+
+function sayName(name){
+
+  if(typeof name !== 'string') {
+    throw new TypeError("Invalid name! Must be a string!")
+  }
+  console.log(name);
+}
 // tests
-sayName("Alex");
-sayName(1);
-// Your code here 
+
+// sayName(1);
+
+try {
+// sayName("Alex");
+// sayName(1);
+} catch(e) {
+  console.log(e.message);
+}
+
+// console.log('do we make it?')
+// Your code here
 
 // 3.
 function greet(greeting) {
+  // console.log(greeting)
   if (!greeting) {
     throw new Error("There was no greeting given.");
   }
@@ -25,3 +55,10 @@ function greet(greeting) {
   console.log(greeting);
 }
 
+
+try{
+  // greet('hello');
+  greet(greeting);
+} catch(error) {
+  console.log('hello world');
+}
