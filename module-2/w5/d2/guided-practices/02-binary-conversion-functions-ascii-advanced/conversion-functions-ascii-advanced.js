b70ed6c8465a072969eb860b0200114bc0d74994
brandon <brandonlaursen95@yahoo.com>
2024-07-30 13:13:43 -0400

    conversions

--- module-2/w5/d2/guided-practices/02-binary-conversion-functions-ascii-advanced/conversion-functions-ascii-advanced.js
+++ module-2/w5/d2/guided-practices/02-binary-conversion-functions-ascii-advanced/conversion-functions-ascii-advanced.js
@@ -9,48 +9,84 @@ Consult documentation on how to utilize the following methods:
 
 /* Base 2 to base 16 */
 function binaryToHexadecimal(blob) {
-  // Your code here 
-}
+  // Your code here
+
+  return '0x' + parseInt(blob.slice(2), 2).toString(16);
+};
+
+
+// console.log('Binary to hexadecimal:')
+// console.log(binaryToHexadecimal('0b 1010')) // '0xa'
+// console.log(binaryToHexadecimal('0b0011')) // '0x3'
+// console.log('')
 
 /* Base 16 to base 2 */
 function hexadecimalToBinary(blob) {
-  // Your code here 
-}
 
-/* Base 10 to ASCII */
-function decimalToAscii(blob) {
-  // Your code here 
-}
-
-/* Base 2 to ASCII */
-function binaryToAscii(blob) {
-  // Your code here 
-}
-
-/* Base 16 to ASCII */
-function hexadecimalToAscii(blob) {
-  // Your code here 
-}
-
-/* ASCII to base 10 */
-function asciiToDecimal(blob) {
-  // Your code here 
-}
-
-// console.log('Binary to hexadecimal:')
-// console.log(binaryToHexadecimal('0b1010')) // '0xa'
-// console.log(binaryToHexadecimal('0b0011')) // '0x3'
-// console.log('')
+  return `0b${parseInt(blob.substring(2), 16).toString(2)}`;
+};
 
 // console.log('Hexadecimal to binary:')
 // console.log(hexadecimalToBinary('0xa1'))  // '0b10100001'
 // console.log(hexadecimalToBinary('0xff'))  // '0b11111111'
 // console.log('')
 
-// console.log('Decimal to ASCII:')
+/* Base 10 to ASCII */
+function decimalToAscii(blob) {
+  // Your code here
+  return String.fromCharCode(blob);
+};
+
+// console.log('Decimal to ASCII:');
 // console.log(decimalToAscii(65))  // 'A'
 // console.log(decimalToAscii(97))  // 'a'
 // console.log('')
+
+/* Base 2 to ASCII */
+function binaryToAscii(blob) {
+  // Your code here
+  return String.fromCharCode(blob);
+};
+
+// console.log('Binary to ASCII:')
+// console.log(binaryToAscii('0b01000010'))  // 'B'
+// console.log(binaryToAscii('0b01100010'))  // 'b'
+// console.log('')
+
+/* Base 16 to ASCII */
+function hexadecimalToAscii(blob) {
+  // Your code here
+
+  return String.fromCharCode(blob);
+};
+
+// console.log('Hexadecimal to ASCII:')
+// console.log(hexadecimalToAscii('0x43'))  // 'C'
+// console.log(hexadecimalToAscii('0x63'))  // 'c'
+// console.log('')
+
+
+/* ASCII to base 10 */
+function asciiToDecimal(blob) {
+  // Your code here
+    // console.log(blob);
+    let res = [];
+    for(let i = 0; i < blob.length; i++) {
+      // let char = blob[i];
+      // console.log(char);
+      res.push(blob.charCodeAt(i));
+    };
+    return res;
+}
+// console.log('ASCII to decimal:')
+// console.log(asciiToDecimal('HELLO'))   // [ 72, 69, 76, 76, 79 ]
+// console.log(asciiToDecimal('world!'))  // [ 119, 111, 114, 108, 100, 33 ]
+
+
+
+
+
+
 
 // console.log('Binary to ASCII:')
 // console.log(binaryToAscii('0b01000010'))  // 'B'
