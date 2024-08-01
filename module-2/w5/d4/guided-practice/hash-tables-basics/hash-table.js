5f54e6e2de758f0cd9cdcedb3310d8a805b55804
brandon <brandonlaursen95@yahoo.com>
2024-08-01 11:38:04 -0400

    hashTables

--- module-2/w5/d4/guided-practice/hash-tables-basics/hash-table.js
+++ module-2/w5/d4/guided-practice/hash-tables-basics/hash-table.js
@@ -1,4 +1,4 @@
-const sha256 = require('js-sha256');
+const sha256 = require('js-sha256');// npm i sha256
 
 class KeyValuePair {
   constructor(key, value) {
@@ -11,27 +11,40 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here 
+    this.count = 0;
+    this.capacity = numBuckets;
+    //* we initialize an array of a fixed size for data
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
-    // Your code here 
+    // Your code here
+    // return 559aead0
   }
 
   hashMod(key) {
-    // Your code here 
+    // Your code here
   }
 
   insertNoCollisions(key, value) {
-    // Your code here 
+    // Your code here
+    // grab the index of the key by running through hashMod
+    // check if there is a pair at the index
+    // this.data[index]
+    // if there is something there
+    // that means we have a collision throw an error
+    // if something isnt there
+    // so well want to create a keyValuePair
+    // and place it at that index
+    // increment count
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here 
+    // Your code here
   }
 
   insert(key, value) {
-    // Your code here 
+    // Your code here
   }
 
 }
