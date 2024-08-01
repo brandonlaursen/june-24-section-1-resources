5b59da888b6d31f349b25e88cc79755c5f5a2cb4
brandon <brandonlaursen95@yahoo.com>
2024-08-01 12:37:58 -0400

    phase 1 + 2

--- module-2/w5/d4/guided-practice/hash-tables-basics/hash-table.js
+++ module-2/w5/d4/guided-practice/hash-tables-basics/hash-table.js
@@ -1,4 +1,4 @@
-const sha256 = require('js-sha256');// npm i sha256
+const sha256 = require("js-sha256"); // npm i sha256
 
 class KeyValuePair {
   constructor(key, value) {
@@ -9,7 +9,6 @@ class KeyValuePair {
 }
 
 class HashTable {
-
   constructor(numBuckets = 4) {
     this.count = 0;
     this.capacity = numBuckets;
@@ -18,25 +17,35 @@ class HashTable {
   }
 
   hash(key) {
-    // Your code here
-    // return 559aead0
+    // console.log('do we make it', key)
+    const hashString = sha256(key).slice(0, 8);
+    // console.log(hashString);
+
+    return parseInt(hashString, 16); // 745595179
   }
 
   hashMod(key) {
-    // Your code here
+    return this.hash(key) % this.capacity; // 3
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
     // grab the index of the key by running through hashMod
+    const index = this.hashMod(key);
     // check if there is a pair at the index
+    if (this.data[index]) {
+      throw new Error("hash collision or same key/value pair already exists!");
+    } else {
+      const newPair = new KeyValuePair(key, value); // {key, value};
+      this.data[index] = newPair;
+      this.count++;
+    }
     // this.data[index]
     // if there is something there
     // that means we have a collision throw an error
     // if something isnt there
     // so well want to create a keyValuePair
     // and place it at that index
-    // increment count
+    // increment
   }
 
   insertWithHashCollisions(key, value) {
@@ -46,8 +55,16 @@ class HashTable {
   insert(key, value) {
     // Your code here
   }
-
 }
 
+// const ht = new HashTable();
+// ht.insertNoCollisions("key", "value"); // 3
+// ht.insertNoCollisions("key1", "value"); // 2
+// ht.insertNoCollisions("key2", "value"); // 2
+// console.log(ht);
+// console.log(ht);
+// console.log(ht.hashMod('key'));// 3
+// console.log(ht.hashMod('key1'));// 2
+// console.log(ht.hashMod('key2'));// 2
 
 module.exports = HashTable;
