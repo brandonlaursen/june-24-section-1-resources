3334957797e257e406e7df9a02d63d67e0b7c135
brandon <brandonlaursen95@yahoo.com>
2024-08-01 13:32:02 -0400

    psuedocode

--- module-2/w5/d4/guided-practice/hash-tables-basics/hash-table.js
+++ module-2/w5/d4/guided-practice/hash-tables-basics/hash-table.js
@@ -39,30 +39,44 @@ class HashTable {
       this.data[index] = newPair;
       this.count++;
     }
-
   }
 
   insertWithHashCollisions(key, value) {
-
     const newPair = new KeyValuePair(key, value); // {key, value};
-     // grab the index of the key by running through hashMod
-     const index = this.hashMod(key);
-     // check if there is a pair at the index
-     if (this.data[index]) {
-      //  throw new Error("hash collision or same key/value pair already exists!");
+    // grab the index of the key by running through hashMod
+    const index = this.hashMod(key);
+    // check if there is a pair at the index
+    if (this.data[index]) {
+
       // set the newPairs next as the pair at that index
+      newPair.next = this.data[index];
       // set the pair at the index to be the newNode
-     } else {
+      this.data[index] = newPair;
 
-       this.data[index] = newPair;
+    } else {
+      this.data[index] = newPair;
+    }
 
-     };
-
-     this.count++;
+    this.count++;
   }
 
   insert(key, value) {
-    // Your code here
+    // find the index by running the key through the hashMod function
+    // grab the pair at the index
+
+    // check if there currentPair at that index is the key we are trying to insert
+      // if it is stop
+      // if it isnt, check the next pair
+
+    // check if the currentPair with they key were look for is there
+      // if it is, overwrite the value
+
+    // If there isnt a pair with key were looking for
+    // create a new KeyValuePair
+
+    // set the newPairs next to be the pair currently at that index
+    // set the pair at that index to be the newPair
+    // increment our count
   }
 }
 
