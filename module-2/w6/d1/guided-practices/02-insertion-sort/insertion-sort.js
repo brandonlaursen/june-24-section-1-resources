17b288811df4d511e6b8f605830f04130f2171da
brandon <brandonlaursen95@yahoo.com>
2024-08-05 16:50:26 -0400

    insertion sort

--- module-2/w6/d1/guided-practices/02-insertion-sort/insertion-sort.js
+++ module-2/w6/d1/guided-practices/02-insertion-sort/insertion-sort.js
@@ -1,10 +1,14 @@
 // Insertion Sort out-of-place
 // Do not modify the original array
+
+// Time = O(n^2);
+// Space = O(n)
 function insertionSort(arr) {
   /*
   Pseudocode:
 
   Copy the original array
+
   Create an array to store the sorted values
   While the array is not empty:
   - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
@@ -18,11 +22,42 @@ function insertionSort(arr) {
   Return the sorted array
   */
 
-  // Your code here 
+  let copyArr = [...arr]; // [2]
+
+  const sorted = [];
+
+  while (copyArr.length > 0) {
+    console.log(sorted.join(","));
+    let val = copyArr.pop(); // 0
+
+    sorted.push(null); //[0, 1, 2, 4, 8]
+
+    let i = sorted.length - 1; // 2
+
+    // 0 > 0
+    while (i > 0) {
+      // 1 < 0
+      if (sorted[i - 1] < val) {
+        break;
+      } else {
+        //
+        sorted[i] = sorted[i - 1];
+        i--;
+      }
+    }
+
+    sorted[i] = val;
+  }
+
+  return sorted;
 }
+
+// console.log(insertionSort([2, 4, 0, 8, 1])); //
 
 // In-place Insertion Sort
 // Mutates the original array
+// Time - O(n^2)
+// Space - O(1)
 function insertionSortInPlace(arr) {
   /*
   Pseudocode:
@@ -38,9 +73,43 @@ function insertionSortInPlace(arr) {
   - Insert the unsorted value at the break point
   - Increment the dividing pointer and repeat
   Return the mutated array
+
+
+      [0, 1, 2, 4, 8] = 5
+
+
+
+                   ^
   */
 
-  // Your code here 
-}
+      let divider = 1;// 5
+
+      while(divider < arr.length) {
+        console.log(arr.join(","));
+        let val = arr[divider];//1
+
+        let i = divider;// 3
+
+        //
+        while(i > 0) {
+
+          // 8 < 1
+          if(arr[i - 1] < val) {
+            break;
+          } else {
+            arr[i] = arr[i - 1];
+            i--;
+          }
+        }
+
+        arr[i] = val;
+        divider++;
+      }
+
+      return arr;
+};
+
+
+console.log(insertionSort([2, 4, 0, 8, 1])); //
 
 module.exports = [insertionSort, insertionSortInPlace];
