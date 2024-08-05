955f66d8bba83b8b6e06da7858be087c76964772
brandon <brandonlaursen95@yahoo.com>
2024-08-05 16:59:47 -0400

    selction sort out of place

--- module-2/w6/d1/guided-practices/03-selection-sort/selection-sort.js
+++ module-2/w6/d1/guided-practices/03-selection-sort/selection-sort.js
@@ -1,24 +1,41 @@
 
 
+
+/*
+
+
+*/
+
+// Time - O(n^2);
+// space - O(n)
 function selectionSort(arr) {
 
   // Copy the original array
+  const copy = [...arr];// []
 
   // Create an array to store the sorted values
+  const sorted = [];// [0, 1, 2, 4, 8]
 
   // While the array is not empty...
+  while(copy.length) {
 
-    // Do not move this console.log
     console.log(sorted.join(","));
 
     // Find the index of the minimum value in the unsorted half
+    let minValue = Math.min(...copy);// 8
+    let minIndex = copy.indexOf(minValue);//0
 
     // Save and remove the value at the min index
+    copy.splice(minIndex, 1);//
 
     // Add the min value to the end of the sorted array
+    sorted.push(minValue);
+  }
 
+  return sorted;
 }
 
+console.log(selectionSort([2,4,0, 8, 1]))
 
 
 function selectionSortInPlace(arr) {
