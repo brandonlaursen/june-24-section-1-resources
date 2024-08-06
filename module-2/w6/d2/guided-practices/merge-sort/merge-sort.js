5ab0a444079a5af9df0a6560d80d0b5cab5b8ab9
brandon <brandonlaursen95@yahoo.com>
2024-08-06 13:54:06 -0400

    merge sort

--- module-2/w6/d2/guided-practices/merge-sort/merge-sort.js
+++ module-2/w6/d2/guided-practices/merge-sort/merge-sort.js
@@ -1,34 +1,91 @@
+
+
+
+
 // Merge Sort out-of-place
 // Do not modify the original array
 function mergeSort(arr) {
-
   // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  if(arr.length <= 1) return arr;
+  // If so, it's already sorted: return
 
   // Divide the array in half
+  let midPoint = Math.floor(arr.length / 2);// [2,4]
 
-  // Recursively sort the left half
-  // Recursively sort the right half
 
+  let left = arr.slice(0, midPoint);// [ 2, 4, 6, 8 ]  -> [2,4]  -> [2]
+
+  let right = arr.slice(midPoint);// [1, 3, 5, 7, 9]   -> [6,8]   -> [4]
   // Merge the halves together and return
 
+  // Recursively sort the left half
+  const leftSorted = mergeSort(left);//
+  // Recursively sort the right half
+  const rightSorted = mergeSort(right);
+
+  // console.log(leftSorted, rightSorted);
+  // return [...leftSorted, ...rightSorted]
+  return merge(leftSorted, rightSorted);
 }
+
+// console.log(mergeSort([2, 4, 6, 8, 1, 3, 5, 7, 9]));
 
 
 // Takes in two sorted arrays and returns them merged into one
-function merge(arrA, arrB) {
+/*
+  a = [ 2, 4 ]
+               ^
+  b = [ 6, 8 ]
+               ^
 
+ */
+function merge(arrA, arrB) {
   // Create an empty return array
+  let arr = [];// [2, 4, 6, 8]
 
   // Point to the first value of each array
+  let indexA = 0;
+  let indexB = 0;
+
   // While there are still values in each array...
-    // Compare the first values of each array
-    // Add the smaller value to the return array
-    // Move the pointer to the next value in that array
+  while(indexA < arrA.length || indexB < arrB.length) {
+  // Compare the first values of each array
+    if(indexA === arrA.length || arrA[indexA] > arrB[indexB]) {
+      arr.push(arrB[indexB]);
+      indexB++;
+    } else {
+      arr.push(arrA[indexA]);
+      indexA++;
+    }
 
+  }
+
+  return arr;
+  // Add the smaller value to the return array
+  // Move the pointer to the next value in that array
   // Return the return array
-
 }
 
-module.exports = [merge, mergeSort];
+// console.log(merge([ 2 ], [ 4 ]));
 
+// console.log(mergeSort([2, 4, 6, 8, 1, 3, 5, 7, 9]));
+/*
+
+
+
+
+
+
+                     [2,4,6,8]  [1,3,5,7,9]
+                              ^          ^
+                      [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+                        [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+                        [2,4,6,8,1,3,5,7,9]
+
+
+*/
+
+module.exports = [merge, mergeSort];
