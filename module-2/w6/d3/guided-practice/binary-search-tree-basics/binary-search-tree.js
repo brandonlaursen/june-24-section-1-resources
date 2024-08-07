04a7add64da406e3e3890071bac52acc5a7d75bf
brandon <brandonlaursen95@yahoo.com>
2024-08-07 11:47:28 -0400

    bst

--- module-2/w6/d3/guided-practice/binary-search-tree-basics/binary-search-tree.js
+++ module-2/w6/d3/guided-practice/binary-search-tree-basics/binary-search-tree.js
@@ -10,40 +10,58 @@ class TreeNode {
 class BinarySearchTree {
 
   constructor() {
-    // Your code here 
+    // Your code here
   }
 
   insert(val, currentNode=this.root) {
-    // Your code here 
+    // Your code here
+
+    // check if there a root
+    // if there is not, the new node is the root
+
+    // check the current nodes value
+    // if the new value is less than the current node val
+    // we traverse the left
+      // check if there is left
+      // if there isnt, set the newNode as the currentNodes left
+      // if there is, traverse
+
+    // if the new value is greater than the current node val
+    // we traverse down the right
+     // we traverse the right
+      // check if there is right
+      // if there isnt, set the newNode as the currentNodes right
+      // if there is, traverse
+
   }
 
   search(val) {
-    // Your code here 
+    // Your code here
   }
 
 
   preOrderTraversal(currentNode = this.root) {
-    // Your code here 
+    // Your code here
   }
 
 
   inOrderTraversal(currentNode = this.root) {
-    // Your code here 
+    // Your code here
   }
 
 
   postOrderTraversal(currentNode = this.root) {
-    // Your code here 
+    // Your code here
   }
 
     // Breadth First Traversal - Iterative
   breadthFirstTraversal() {
-    // Your code here 
+    // Your code here
   }
 
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
-    // Your code here 
+    // Your code here
   }
 }
 
