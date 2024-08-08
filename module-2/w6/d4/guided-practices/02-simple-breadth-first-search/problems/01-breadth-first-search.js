2274e65b469b301b8614c98a0e7fdc9758cf7ca4
brandon <brandonlaursen95@yahoo.com>
2024-08-08 12:36:31 -0400

    bfs

--- module-2/w6/d4/guided-practices/02-simple-breadth-first-search/problems/01-breadth-first-search.js
+++ module-2/w6/d4/guided-practices/02-simple-breadth-first-search/problems/01-breadth-first-search.js
@@ -7,22 +7,40 @@ For this exercise, try to write your code from scratch. It's good practice!
 */
 
 const adjList = {
-    1: [2, 5],
-    2: [1, 3, 5],
-    3: [2, 4],
-    4: [3, 5],
-    5: [1, 2, 4],
-    6: []
-}
+  1: [2, 5],
+  2: [1, 3, 5],
+  3: [2, 4],
+  4: [3, 5],
+  5: [1, 2, 4],
+  6: [],
+};
 
 function breadthFirstSearch(start, end) {
-  // Your code here 
+  const queue = [start];
+  const visited = new Set([start]);
+
+  while (queue.length) {
+    const curr = queue.shift();
+
+    // DO THE THING
+    if (curr === end) return true;
+
+    const neighbors = adjList[curr];
+
+    neighbors.forEach((neighbor) => {
+      if (!visited.has(neighbor)) {
+        queue.push(neighbor);
+        visited.add(neighbor);
+      }
+    });
+  }
+
+  return false;
 }
 
 // console.log(breadthFirstSearch(1, 3)); // -> true
 // console.log(breadthFirstSearch(4, 1)); // -> true
 // console.log(breadthFirstSearch(6, 1)); // -> false
-
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = breadthFirstSearch;
