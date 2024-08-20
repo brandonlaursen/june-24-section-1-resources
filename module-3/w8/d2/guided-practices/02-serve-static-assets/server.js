0985a626dd35e74ff1a01542f56df1c597b25418
brandon <brandonlaursen95@yahoo.com>
2024-08-20 14:18:04 -0400

    lunch time

--- module-3/w8/d2/guided-practices/02-serve-static-assets/server.js
+++ module-3/w8/d2/guided-practices/02-serve-static-assets/server.js
@@ -2,7 +2,51 @@ const http = require('http');
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // Your code here 
+
+
+
+  if(req.method === 'GET' && req.url.startsWith('/static')) {
+
+    // console.log(req.url);
+
+    const path = req.url.split('/static')[1];
+    console.log('==>',path);/// /images/dog.jpg
+
+
+    let ext = path.split('.')[1];
+    console.log('ext',ext);/// jpg
+
+    let contentType;
+    if(ext === 'jpg') {
+      contentType = 'image/jpeg'
+    } else if(ext === 'css') {
+      contentType = 'text/css'
+    } else {
+      contentType = 'text/html';
+    }
+
+    console.log('ct',contentType);// image/jpeg
+
+    // ./assets/images/dog.jpg
+    const resBody = fs.readFileSync(`./assets${path}`);
+
+
+
+    res.setHeader('Content-Type', contentType);
+    res.statusCode = 200;
+
+    return res.end(resBody);
+  }
+
+
+  const resBody = fs.readFileSync('./index.html');
+
+  res.setHeader('Content-Type', 'text/html');
+  res.statusCode = 200;
+
+  return res.end(resBody);
+
+
 });
 
 const port = 5000;
