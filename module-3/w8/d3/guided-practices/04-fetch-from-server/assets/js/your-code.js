8284f6ed6cbd306165ee65f13ec6a7be4a899ccc
brandon <brandonlaursen95@yahoo.com>
2024-08-21 13:47:12 -0400

    practice 4

--- module-3/w8/d3/guided-practices/04-fetch-from-server/assets/js/your-code.js
+++ module-3/w8/d3/guided-practices/04-fetch-from-server/assets/js/your-code.js
@@ -1,5 +1,3 @@
-
-
 /*
 
     Fetch
@@ -13,34 +11,60 @@
 
 */
 
-
 export function getAllDogs() {
-    // Your code here
+  // Your code here
 
-    return fetch('/dogs', {
-        method: "GET",
-        headers: {"Content-Type": null},
-        body: null
-    });
-    // return fetch('/dogs', {
-    //     method: "GET",
-    //     headers: {"Content-Type": null},
-    //     body: null
-    // })
+  // return fetch('/dogs', {
+  //     method: "GET",
+  //     headers: {"Content-Type": null},
+  //     body: null
+  // });
+
+  return fetch("/dogs");
+  // return fetch('/dogs', {
+  //     method: "GET",
+  //     headers: {"Content-Type": null},
+  //     body: null
+  // })
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+  // Your code here
+
+  return fetch("/dogs/2");
 }
 
 export function postNewDog() {
-    // Your code here
+  return fetch("/dogs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      name: "little bear",
+      age: 17,
+    }),
+  });
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+  return fetch("/dogs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      name,
+      age,
+    }),
+  });
 }
 
 export function deleteDog(id) {
-      // Your code here
+  return fetch(`/dogs/${id}/delete`, {
+    method: "POST",
+    headers: {
+      AUTH: "ckyut5wau0000jyv5bsrud90y",
+    },
+  });
 }
