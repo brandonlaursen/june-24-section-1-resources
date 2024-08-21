86bffdde93928e5450e31e78fce936aad7edf2f5
brandon <brandonlaursen95@yahoo.com>
2024-08-21 13:02:26 -0400

    phase 1

--- module-3/w8/d3/guided-practices/01-create-and-handle-promises/phase-1.js
+++ module-3/w8/d3/guided-practices/01-create-and-handle-promises/phase-1.js
@@ -1,19 +1,40 @@
 function stretch() {
-  // Your code here 
+  return new Promise((res) => {
+    setTimeout(() => {
+      console.log("done stretching");
+      res();
+    }, 1000);
+  });
 }
 
 function runOnTreadmill() {
-  // Your code here 
+  return new Promise((res) => {
+    setTimeout(() => {
+      console.log("done running on treadmill");
+      res();
+    }, 500);
+  });
 }
 
 function liftWeights() {
-  // Your code here 
+  return new Promise((res) => {
+    setTimeout(() => {
+      console.log("done lifting weights");
+      res();
+    }, 2000);
+  });
 }
 
 function workout() {
-  // Your code here 
-}
+  // Your code here
 
+  stretch()
+    .then(() => runOnTreadmill())
+    .then(() => liftWeights())
+    .then(() => console.log("done working out!"));
+
+
+}
 
 /* ============================ TEST YOUR CODE ============================
 
@@ -21,10 +42,9 @@ Run the file (`node phase-1.js`) and check your output against the expected
 output.
 */
 
-
 workout();
-  // should print out the following:
-    // done stretching
-    // done running on treadmill
-    // done lifting weights
-    // done working out
+// should print out the following:
+// done stretching
+// done running on treadmill
+// done lifting weights
+// done working out
