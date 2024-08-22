a6ed9999cf199f528d9511c14d267d9dd6619b9e
brandon <brandonlaursen95@yahoo.com>
2024-08-22 15:59:09 -0400

    phase 2

--- module-3/w8/d4/guided-practices/04-create-api-endpoints/server.js
+++ module-3/w8/d4/guided-practices/04-create-api-endpoints/server.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const http = require("http");
 
 const dogs = [
   {
     dogId: 1,
     name: "Fluffy",
-    age: 2
-  }
+    age: 2,
+  },
 ];
 
 let nextDogId = 2;
@@ -25,10 +25,11 @@ const server = http.createServer((req, res) => {
     reqBody += data;
   });
 
-  req.on("end", () => { // request is finished assembly the entire request body
+  req.on("end", () => {
+    // request is finished assembly the entire request body
     // Parsing the body of the request depending on the Content-Type header
     if (reqBody) {
-      switch (req.headers['content-type']) {
+      switch (req.headers["content-type"]) {
         case "application/json":
           req.body = JSON.parse(reqBody);
           break;
@@ -52,13 +53,13 @@ const server = http.createServer((req, res) => {
     /* ======================== ROUTE HANDLERS ======================== */
 
     // GET /dogs
-    if (req.method === 'GET' && req.url === '/dogs') {
+    if (req.method === "GET" && req.url === "/dogs") {
       // Your code here
 
       // set the status code
       res.statusCode = 200;
       // set the headers;
-      res.setHeader("Content-Type", 'application/json');
+      res.setHeader("Content-Type", "application/json");
       // set the body - send the body as json - JSON.stringify
 
       let json = JSON.stringify(dogs);
@@ -68,25 +69,37 @@ const server = http.createServer((req, res) => {
     }
 
     // GET /dogs/:dogId
-    if (req.method === 'GET' && req.url.startsWith('/dogs/')) {
-      const urlParts = req.url.split('/'); // ['', 'dogs', '1']
+    if (req.method === "GET" && req.url.startsWith("/dogs/")) {
+      const urlParts = req.url.split("/"); // ['', 'dogs', '1']
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
-        // Your code here
+        // console.log(dogId);
+
+        const foundDog = dogs.find((dog) => dogId == dog.dogId);
+        // console.log(foundDog);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        if (foundDog) {
+          return res.end(JSON.stringify(foundDog));
+        } else {
+          return res.end(JSON.stringify({ error: "no dog with id" }));
+        }
       }
-      return res.end();
     }
 
     // POST /dogs
-    if (req.method === 'POST' && req.url === '/dogs') {
+    if (req.method === "POST" && req.url === "/dogs") {
       const { name, age } = req.body;
       // Your code here
       return res.end();
     }
 
     // PUT or PATCH /dogs/:dogId
-    if ((req.method === 'PUT' || req.method === 'PATCH')  && req.url.startsWith('/dogs/')) {
-      const urlParts = req.url.split('/');
+    if (
+      (req.method === "PUT" || req.method === "PATCH") &&
+      req.url.startsWith("/dogs/")
+    ) {
+      const urlParts = req.url.split("/");
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
         // Your code here
@@ -95,8 +108,8 @@ const server = http.createServer((req, res) => {
     }
 
     // DELETE /dogs/:dogId
-    if (req.method === 'DELETE' && req.url.startsWith('/dogs/')) {
-      const urlParts = req.url.split('/');
+    if (req.method === "DELETE" && req.url.startsWith("/dogs/")) {
+      const urlParts = req.url.split("/");
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
         // Your code here
@@ -106,16 +119,14 @@ const server = http.createServer((req, res) => {
 
     // No matching endpoint
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'application/json');
-    return res.end('Endpoint not found');
+    res.setHeader("Content-Type", "application/json");
+    return res.end("Endpoint not found");
   });
-
 });
 
-
 if (require.main === module) {
-    const port = 8000;
-    server.listen(port, () => console.log('Server is listening on port', port));
+  const port = 8000;
+  server.listen(port, () => console.log("Server is listening on port", port));
 } else {
-    module.exports = server;
+  module.exports = server;
 }
