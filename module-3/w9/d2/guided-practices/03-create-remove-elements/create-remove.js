5a0ba996e19f2d95262dafbcf28ecf646d38f340
brandon <brandonlaursen95@yahoo.com>
2024-08-27 13:22:53 -0400

    getting started

--- module-3/w9/d2/guided-practices/03-create-remove-elements/create-remove.js
+++ module-3/w9/d2/guided-practices/03-create-remove-elements/create-remove.js
@@ -1,5 +1,6 @@
 /****************************** ADD DOG BUTTON ******************************/
 const add = document.getElementById("add");
+
 add.addEventListener("click", async () => {
     try {
         const res = await fetch("https://dog.ceo/api/breeds/image/random")
@@ -8,15 +9,42 @@ add.addEventListener("click", async () => {
         const url = data.message; // URL of new dog image
 
         /*--------------- Get breed (Hint: Parse from URL) ---------------- */
-        // Your code here 
+        // Your code here
 
+        // create li
+        const newDog = document.createElement('li');
+
+        // create figure
+        // create image
+        // set the image.src = url
+        // create the caption
+        // set the caption inner text = breed
+
+        // append the image to the figure
+        // append the figcaption to the figure
+        // append the figure to the li
+        // append the li to ul
+
+        // use selector to find the ul
+        // than append li to ul using .appendChild()
+
+/*
+
+
+
+
+
+
+
+
+*/
         /*------------ Create new dog card with the url above ------------- */
         /* (use the HTML structure for the current dog image in the index.html
             file to create a new image with the url) */
-        // Your code here 
+        // Your code here
 
         /* Add the new dog card as a child to the ul in the .gallery element */
-        // Your code here 
+        // Your code here
 
     } catch (e) {
         console.log("Couldn't fetch dog :(")
@@ -27,18 +55,19 @@ add.addEventListener("click", async () => {
 const removeFirst = document.getElementById("remove-first");
 removeFirst.addEventListener("click", () => {
     /*-------------------- Select the first dog card --------------------- */
-    // Your code here 
+    // Your code here
+    // .remove()
 
     /*-------------------- Remove the first dog card --------------------- */
-    // Your code here 
+    // Your code here
 });
 
 /************************** REMOVE LAST DOG BUTTON ***************************/
 const removeLast = document.getElementById("remove-last");
 removeLast.addEventListener("click", () => {
     /*-------------------- Select the last dog card ----------------------- */
-    // Your code here 
+    // Your code here
 
     /*-------------------- Remove the last dog card ----------------------- */
-    // Your code here 
+    // Your code here
 });
