a319765075407ce0dafdcbf1dde84d728838d84d
brandon <brandonlaursen95@yahoo.com>
2024-08-27 13:05:01 -0400

    selectors

--- module-3/w9/d2/guided-practices/02-element-selection/selectors.js
+++ module-3/w9/d2/guided-practices/02-element-selection/selectors.js
@@ -19,39 +19,65 @@ const select = () => {
 
     /* Section 1 */
     // 1. Get all seeded fruit elements
-    // Your code here
     const seeded = document.getElementsByClassName("seed");
+    // console.log("seeded:", seeded);
 
-    // console.log(seeded);
+    // [...seeded].forEach(element => console.log(element.innerText));
+
 
     // 2. Get all seedless fruit elements
-    // Your code here
+    const seedless = document.getElementsByClassName("seedless");
+    // console.log("seedless:", seedless);
+
+    // [...seedless].forEach(element => console.log(element.innerText));
+
 
     // 3. Get first seedless fruit element
-    // Your code here
+    const firstSeedless = seedless[0];
+    // console.log("firstSeedless:", firstSeedless);
+
 
     /* Section 2 */
     // 4. Get inner span with text "you"
-    // Your code here
+    const you = document.querySelector('#wrapper span');
+    // console.log("you:", you);
 
     // 5. Get all children of element "wrapper"
-    // Your code here
+    const children = document.getElementById('wrapper').children;
+    // console.log(children);
 
     // 6. Get all odd number list items in the list
-    // Your code here
+    const odd = document.getElementsByClassName("odd");
+    // console.log(odd);
 
     // 7. Get all even number list items in the list
-    // Your code here
+    const evenItems = document.querySelectorAll(".odd + li");
+    // console.log(evenItems);
+
+    const evenListItems = document.querySelectorAll('ol li:not(.odd)');
+
+    const even = document.querySelectorAll('li:nth-child(even)');
 
     /* Section 3 */
     // 8. Get all tech companies without a class name
-    // Your code here
+    const techsWithoutClass = document.querySelectorAll("a:not([class])");
+    console.log("techsWithoutClass:", techsWithoutClass);
 
     // 9. Get "Amazon" list element
-    // Your code here
+    const amazon = document.querySelector('.shopping');
+    // console.log("amazon:", amazon);
+
 
     // 10. Get all unicorn list elements (not the image element)
-    // Your code here
+
+    const unicornList = document.querySelectorAll('.unicorn')
+    // console.log(unicornList);
+    unicornList.forEach(node => console.log(node.parentElement));
+
+    const unicornListItems = document.querySelectorAll('ul li img.unicorn');
+    // console.log(unicornList);
+
+
 }
 
 window.onload = select;
