dfe82a06c51f1667930c6d107d131d9f9c7f9590
brandon <brandonlaursen95@yahoo.com>
2024-09-03 18:51:02 -0400

    errors

--- module-4/W10/D2/02-express-error-handling-404/server/app.js
+++ module-4/W10/D2/02-express-error-handling-404/server/app.js
@@ -5,5 +5,31 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
+
+
+app.use((req, res, next) => {
+  const error = new Error("Sorry, the requested resource couldn't be found");
+
+  error.status  = 404;
+  // console.log(error.message)
+  next(error);
+});
+
+
+app.use((err, req, res, next) => {
+
+    // const status = err.status;
+    console.log(err);
+    res.status(err.status);
+
+    // res.message =  "Sorry, the requested resource couldn't be found.";
+
+    res.json({
+      message: err.message,
+      status: err.status
+    });
+})
+
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
