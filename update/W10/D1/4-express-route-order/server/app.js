82c52a484ea47f13807d8c962f6016de9052d0c7
brandon <brandonlaursen95@yahoo.com>
2024-09-03 13:52:17 -0400

    update

--- update/W10/D1/4-express-route-order/server/app.js
+++ update/W10/D1/4-express-route-order/server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+
+
 app.get('/', (req, res) => {
     res.send("Server is alive");
 });
@@ -9,17 +11,19 @@ app.get('/hello', (req, res) => {
     res.send("Hello, my friend!");
 });
 
+app.get('/goodbye/until/forever', (req, res) => {
+    res.send("So long. Farewell. Have a great life!");
+});
+
+app.get('/goodbye/until/:time', (req, res) => {
+    res.send(`Have a nice day! See you ${req.params.time}.`);
+});
+
 app.get(['/goodbye', '/goodbye/*'], (req, res) => {
     res.send("Goodbye, my friend!");
 });
 
-app.get('/goodbye/until/:time', (req, res) => {
-    res.send(`Goodbye. See you ${req.params.time}.`);
-});
 
-app.get('/goodbye/until/forever', (req, res) => {
-    res.send("So long. Farewell. Have a great life!");
-});
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
