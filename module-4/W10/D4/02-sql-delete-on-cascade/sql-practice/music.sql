51331f28b4406cf43bade27bdfa13692b6683fec
brandon <brandonlaursen95@yahoo.com>
2024-09-05 13:13:05 -0400

    queries

--- module-4/W10/D4/02-sql-delete-on-cascade/sql-practice/music.sql
+++ module-4/W10/D4/02-sql-delete-on-cascade/sql-practice/music.sql
@@ -1,6 +1,10 @@
+
+.mode box
+
 PRAGMA foreign_keys = ON;
 DROP TABLE IF EXISTS musicians;
 DROP TABLE IF EXISTS bands;
+
 CREATE TABLE bands (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(100)
@@ -11,8 +15,10 @@ CREATE TABLE musicians (
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100),
   band_id INTEGER,
-  FOREIGN KEY (band_id) REFERENCES bands(id)
+  FOREIGN KEY (band_id) REFERENCES bands(id) ON DELETE CASCADE
 );
+-- ON DELETE CASCADE
+-- ON DELETE SET NULL
 INSERT INTO bands
 VALUES (1, 'The Falling Box'),
   (2, 'America The Piano'),
@@ -30,3 +36,24 @@ VALUES (1, 'Adam', 'Appleby', 1),
   (8, 'Camila', 'Nenci', 4),
   (9, 'Rosemarie', 'Affini', 5),
   (10, 'Victoria', 'Cremonesi', 5);
+
+
+-- SELECT * FROM musicians;
+-- SELECT * FROM bands;
+
+-- DELETE FROM bands
+-- WHERE id = 1;
+
+-- SELECT * FROM musicians;
+-- SELECT * FROM bands;
+
+SELECT * FROM bands WHERE id >= 3;
+
+SELECT * FROM bands WHERE id BETWEEN 1 AND 3;
+
+SELECT * FROM musicians WHERE id IN (1,2,3);
+
+SELECT * FROM musicians
+ORDER BY id DESC
+LIMIT 5
+OFFSET 2
