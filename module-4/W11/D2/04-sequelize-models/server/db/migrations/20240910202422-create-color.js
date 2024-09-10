0bc22616899fff36a08666e1b91ad0e3cdca646b
brandon <brandonlaursen95@yahoo.com>
2024-09-10 17:08:36 -0400

    models

--- module-4/W11/D2/04-sequelize-models/server/db/migrations/20240910202422-create-color.js
+++ module-4/W11/D2/04-sequelize-models/server/db/migrations/20240910202422-create-color.js
@@ -11,7 +11,7 @@ module.exports = {
       name: {
         type: Sequelize.STRING,
         allowNull: false,
-        unique: true
+        unique: true,
       },
       createdAt: {
         allowNull: false,
