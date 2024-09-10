7a70083e1227c2b41976b57f63205dcc27eb57c1
brandon <brandonlaursen95@yahoo.com>
2024-09-10 13:36:57 -0400

    practice 2

---
+++ module-4/W11/D2/aa11-sequelize-create-migration/server/db/migrations/20240910172822-create-color.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("Colors", {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: Sequelize.STRING,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('Colors')
+  },
+};
